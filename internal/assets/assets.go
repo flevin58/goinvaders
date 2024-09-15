0722d6bdb46028a73d85f065c8dd026501758ad8
flevin58 <flevin58@gmail.com>
2024-09-15 13:27:12 +0200

    all assets are now embedded in the executable

--- internal/assets/assets.go
+++ internal/assets/assets.go
@@ -2,6 +2,7 @@ package assets
 
 import (
 	"embed"
+	"os"
 	"path"
 
 	rl "github.com/gen2brain/raylib-go/raylib"
@@ -10,23 +11,37 @@ import (
 //go:embed fonts images sounds
 var assetFS embed.FS
 
+func getEmbeddedData(filename string) []byte {
+	data, err := assetFS.ReadFile(filename)
+	if err != nil {
+		rl.TraceLog(rl.LogError, "Could not load %s: %s", filename, err.Error())
+		os.Exit(1)
+	}
+	return data
+}
+
 func LoadFont(filename string) rl.Font {
-	filename = path.Join("internal", "assets", "fonts", filename)
-	return rl.LoadFontEx(filename, 64, nil, 0)
+	filename = path.Join("fonts", filename)
+	data := getEmbeddedData(filename)
+	return rl.LoadFontFromMemory(".ttf", data, 64, nil)
 }
 
 func LoadTexture(filename string) rl.Texture2D {
-	filename = path.Join("internal", "assets", "images", filename)
-	//filename = path.Join("images", filename)
-	return rl.LoadTexture(filename)
+	filename = path.Join("images", filename)
+	data := getEmbeddedData(filename)
+	image := rl.LoadImageFromMemory(".png", data, int32(len(data)))
+	return rl.LoadTextureFromImage(image)
 }
 
 func LoadMusic(filename string) rl.Music {
-	filename = path.Join("internal", "assets", "sounds", filename)
-	return rl.LoadMusicStream(filename)
+	filename = path.Join("sounds", filename)
+	data := getEmbeddedData(filename)
+	return rl.LoadMusicStreamFromMemory(".ogg", data, int32(len(data)))
 }
 
 func LoadSound(filename string) rl.Sound {
-	filename = path.Join("internal", "assets", "sounds", filename)
-	return rl.LoadSound(filename)
+	filename = path.Join("sounds", filename)
+	data := getEmbeddedData(filename)
+	wave := rl.LoadWaveFromMemory(".ogg", data, int32(len(data)))
+	return rl.LoadSoundFromWave(wave)
 }
